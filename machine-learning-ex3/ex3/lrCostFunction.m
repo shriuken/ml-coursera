0a183da7e8e4068d20b37bb21dc54d01c53fc7eb
Ryan Benner <bennerrj@outlook.com>
2018-05-09 07:04:03 -0400

    implement cost function for ex3

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -37,7 +37,11 @@ grad = zeros(size(theta));
 %
 
 
+J = (1 / m) * sum( -y .* log(sigmoid(X * theta)) - (1 .- y) .* log(1 .- sigmoid(X * theta)) ) + ( (lambda / (2 * m)) * sum(theta(2:end).^2));
+grad(1, 1) = (1 / m ) * sum(( sigmoid(X * theta) - y) .* X(:, 1) );
 
+% (lambda / m ) * theta(2:end) -- should be fine.
+grad(2:end, 1) = (1 / m) * sum(( sigmoid(X * theta) - y) .* X(:, 2:end) )' + (lambda / m) * theta(2:end) ;
 
 
 
