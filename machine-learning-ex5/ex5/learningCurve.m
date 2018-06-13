6034a423bd7806d1a9567c14e64b8e90740e438c
Ryan Benner <rbenner@gaig.com>
2018-06-13 08:12:09 -0400

    finished hw5

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -1,5 +1,5 @@
 function [error_train, error_val] = ...
-    learningCurve(X, y, Xval, yval, lambda, theta)
+    learningCurve(X, y, Xval, yval, lambda)
 %LEARNINGCURVE Generates the train and cross validation set errors needed 
 %to plot a learning curve
 %   [error_train, error_val] = ...
@@ -58,7 +58,7 @@ error_val   = zeros(m, 1);
 
 for i = 1:m
     theta = trainLinearReg(X(1:i, :), y(1:i, :), lambda);
-    error_train(i) = linearRegCostFunction(X(1:i, :), y(1:i, :), theta, lambda);
+    error_train(i) = linearRegCostFunction(X(1:i, :), y(1:i, :), theta, 0);
     error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
 end
 
