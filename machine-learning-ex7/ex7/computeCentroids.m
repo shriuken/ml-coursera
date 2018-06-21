e5d56410e1053056d2a5ddc5c2691064d9c3b627
Ryan Benner <bennerrj@outlook.com>
2018-06-21 07:08:34 -0400

    finished compute centroid means

--- machine-learning-ex7/ex7/computeCentroids.m
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -16,7 +16,6 @@ function centroids = computeCentroids(X, idx, K)
 % You need to return the following variables correctly.
 centroids = zeros(K, n);
 
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: Go over every centroid and compute mean of all points that
 %               belong to it. Concretely, the row vector centroids(i, :)
@@ -26,8 +25,13 @@ centroids = zeros(K, n);
 % Note: You can use a for-loop over the centroids to compute this.
 %
 
+for i=1:m
+  centroids(idx(i), :) = centroids(idx(i), :) + X(i, :);
+end
 
-
+for j=1:K
+  centroids(j, :) = centroids(j, :) / nnz(idx == j);
+end
 
 
 
